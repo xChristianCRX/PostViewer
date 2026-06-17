69fccc843b8c87b0a37ef1e12e0a21ecedd72d95
xChristianCRX <riccichristian338@gmail.com>
2026-06-16 22:50:02 -0300

    feat: adiciona classe PostViewModel

---
+++ app/src/main/java/br/edu/ifsp/scl/sc3044572/postviewer/PostViewModel.kt
@@ -0,0 +1,7 @@
+package br.edu.ifsp.scl.sc3044572.postviewer
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class PostViewModel(application: Application): AndroidViewModel(application) {
+}
