52773c9b10081d150170d82ef43f2c57684fe124
xChristianCRX <riccichristian338@gmail.com>
2026-06-17 00:13:15 -0300

    refactor: uma unica classe para Post e Comment

---
+++ app/src/main/java/br/edu/ifsp/scl/sc3044572/postviewer/model/Models.kt
@@ -0,0 +1,17 @@
+package br.edu.ifsp.scl.sc3044572.postviewer.model
+
+data class Post(
+    val userId: Int,
+    val id: Int,
+    val title: String,
+    val body: String
+)
+
+data class Comment(
+    val postId: Int,
+    val id: Int,
+    val name: String,
+    val email: String,
+    val body: String,
+    val isLocal: Boolean = false
+)
