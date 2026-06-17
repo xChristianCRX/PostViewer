7d985aa671526950799dac0a8822507ccf67dc1e
xChristianCRX <riccichristian338@gmail.com>
2026-06-16 23:16:14 -0300

    config: adiciona gson dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.google.gson)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
